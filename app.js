73e5229788f7da5f5b4d8cb0c47e978214b697bf
bilalkk9 <you@example.combilalkk63@gmail.com>
2020-06-30 15:45:34 +0500

    changed ui and added bootstrap

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var inputValue=document.querySelector('.inputValue');
 var place=document.querySelector('.place');
 var desc=document.querySelector('.desc');
 var temp=document.querySelector('.temp');
+var humid=document.querySelector('.humid');
 
 button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=0f14da7e696064f8f46bcc9332e8909d')
@@ -25,11 +26,24 @@ button.addEventListener('click',function(){
     .then(data=>{
             var nameValue=data['name'];
             var tempValue=data['main']['temp'];
-            var descValue=data['weather'][0]['main'];
+            var descValue=data['weather'][0]['description'];
+            var humidValue=data['main']['humidity'];
         
             place.innerHTML=nameValue;
             temp.innerHTML=tempValue;
             desc.innerHTML=descValue;
+            humid.innerHTML=humidValue;
+
+            //celsius formula
+            var celsius=tempValue-273;
+
+            temp.addEventListener('click', ()=>{
+                if(temp.textContent==tempValue){
+                    temp.textContent=Math.floor(celsius);
+                }else{
+                    temp.textContent=tempValue;
+                }                
+            })
         })
 
     .catch(err=>alert("Invalid city name!"));
